e4ab0a8b33febf2babbead39456febaf4368af7d
HaruTzuki <dimitris.v.91@hotmail.com>
2020-03-13 18:58:08 +0200

    Adding some pictures, db and Component folder

---
+++ Hotel_Booking/Components/Serialization/JsonSerialize.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hotel_Booking.Components.Serialization
+{
+    public class JsonSerialize
+    {
+        public static T JsonObjectDeserialize<T>(string JsonText)
+        {
+            return JsonConvert.DeserializeObject<T>(JsonText);
+        }
+
+        public static string JsonObjectSerialize(object? Obj)
+        {
+            return JsonConvert.SerializeObject(Obj);
+        }
+    }
+}
